435af55ff3ce11ac4b58f4fd0b09158abd15a231
Jonathan Hang <hangjo@engr.orst.edu>
2022-07-01 11:01:37 -0500

    Created a '__test__' folder to contain all of our test, added a new script in package.json to work with 'npm run test:update-snapshots'

--- __test__/App.test.js
+++ __test__/App.test.js
@@ -1,7 +1,7 @@
 import "react-native";
 import React from "react";
 import { render, fireEvent, cleanup } from "@testing-library/react-native";
-import App from "./App";
+import App from '../App';
 
 // Source: https://everyday.codes/react-native/iterate-faster-with-github-actions-for-react-native/
 

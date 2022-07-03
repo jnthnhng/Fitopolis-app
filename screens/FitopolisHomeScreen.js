f293ab842dfa5e279e15c7d5fe6380aba588470c
guedead <guedead@oregonstate.edu>
2022-07-02 19:05:36 -0500

    small updates to styles

--- screens/FitopolisHomeScreen.js
+++ screens/FitopolisHomeScreen.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
+
 const FitopolisHomeScreen = () => {
   return (
     <View style={styles.container}>
       <View style={styles.outputTextContainer}>
-        <Text style={styles.completedText}>Challenges Completed</Text>
-        <Text style={styles.badgesText}>Badges Earned</Text>
+        <Text style={styles.box}>username</Text>
+        <Text style={styles.box}>Challenges Completed</Text>
+        {/* <Text style={styles.box}>Badges Earned</Text> */}
       </View>
       <View style={styles.numberContainer}>
         <Text style={styles.challengeNum}>20</Text>
         <Text style={styles.badgeNum}>24</Text>
       </View>
       <View style={styles.buttonContainer}>
-          <Text style={styles.manageText}>Manage Challenges</Text>
+        <Text style={styles.manageText}>Manage Challenges</Text>
         <TouchableOpacity style={styles.button}>
           <Text style={styles.buttonText}>Create</Text>
         </TouchableOpacity>
@@ -33,11 +35,26 @@ const styles = StyleSheet.create({
     backgroundColor: "#e6e4df",
     alignItems: "center",
   },
+  outputTextContainer: {
+    flexDirection: "row",
+    flex: 1,
+    justifyContent: 'space-evenly',
+  },
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'gray',
+    textAlign: 'center',
+  },
+  numberContainer: {
+      flex: 1,
+  },
   buttonContainer: {
     width: "50%",
     justifyContent: "center",
     alignItems: "center",
     marginTop: 40,
+    flex: 3,
   },
   button: {
     backgroundColor: "#3b3a39",
@@ -53,7 +70,7 @@ const styles = StyleSheet.create({
     fontSize: 18,
   },
   manageText: {
-      fontWeight: "600",
-      fontSize: 20
-  }
+    fontWeight: "600",
+    fontSize: 20,
+  },
 });

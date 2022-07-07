25b12ede5fc4cf1532238dc5678b8b01c88601cb
guedead <guedead@oregonstate.edu>
2022-07-07 13:47:27 -0400

    added mock layout for favorites, not connected to backend

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,14 +1,106 @@
-import React, { Component } from "react";
-import { Text, StyleSheet, View } from "react-native";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  FlatList,
+  ScrollView,
+  Image,
+} from "react-native";
 
-export default class FavoritesScreen extends Component {
-  render() {
-    return (
-      <View>
-        <Text> Favorites </Text>
-      </View>
-    );
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
+
+const FavoritesScreen = ({ navigation }) => {
+  function goToCreate() {
+    navigation.navigate("Create");
   }
-}
+  return (
+    <>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Ionicons name="star" size={50} color="gold" />
+          <Text style={styles.favorites}>Favorites</Text>
+        </View>
+        <View style={styles.challengeInfo}>
+          <View style={styles.challengeContainer}>
+            <Text style={styles.stat}>Challenge 1</Text>
+          </View>
+          <View style={styles.challengeContainer}>
+            <Text style={styles.stat}>Challenge 2</Text>
+          </View>
+        </View>
+      </View>
+    </>
+  );
+};
 
-const styles = StyleSheet.create({});
+export default FavoritesScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#e6e4df",
+    alignItems: "center",
+  },
+  header: {
+    alignItems: "center",
+    marginTop: 20,
+  },
+  favorites: {
+    color: "black",
+    fontWeight: "bold",
+    fontSize: 50,
+    marginBottom: 20,
+  },
+  challengeInfo: {
+    flexDirection: "row",
+    flexShrink: 1,
+    paddingTop: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "90%",
+  },
+  challengeContainer: {
+    padding: 5,
+    elevation: 4,
+    backgroundColor: "#c7c7c3",
+    borderRadius: 10,
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    textAlign: "center",
+    flexShrink: 1,
+  },
+  stat: {
+    flexShrink: 1,
+    textAlign: "center",
+    fontWeight: "600",
+    fontSize: 20,
+  },
+  buttonContainer: {
+    flex: 2,
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 40,
+  },
+  button: {
+    backgroundColor: "#3b3a39",
+    width: "70%",
+    padding: 10,
+    borderRadius: 10,
+    alignItems: "center",
+    marginTop: 10,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "600",
+    fontSize: 20,
+  },
+  manageText: {
+    fontWeight: "600",
+    fontSize: 25,
+    paddingBottom: 15,
+  },
+});

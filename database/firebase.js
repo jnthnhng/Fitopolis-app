18b53927ad66ff8e9e918a839b6341ac3f170d4d
guedead <guedead@oregonstate.edu>
2022-07-08 12:59:19 -0400

    connected register screen to firebase auth to create user

--- database/firebase.js
+++ database/firebase.js
@@ -1,22 +1,23 @@
 // database/firebase.js
-import { initializeApp } from 'firebase/app'
-import firebase from 'firebase/compat/app';
+import { initializeApp } from "firebase/app";
+import firebase from "firebase/compat/app";
 import "firebase/storage";
-import { getDatabase, ref, set } from 'firebase/database';
+import { getDatabase, ref, set } from "firebase/database";
+// import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCm7aNK4pL-gHR_0dal40p6viwRu3Lw6V0",
-    authDomain: "fitopolis-72025.firebaseapp.com",
-    projectId: "fitopolis-72025",
-    storageBucket: "fitopolis-72025.appspot.com",
-    messagingSenderId: "672295851701",
-    appId: "1:672295851701:web:578e4f7a83aa834d4b50fc",
-    measurementId: "G-DZK3LR9P9G"
+  apiKey: "AIzaSyCm7aNK4pL-gHR_0dal40p6viwRu3Lw6V0",
+  authDomain: "fitopolis-72025.firebaseapp.com",
+  projectId: "fitopolis-72025",
+  storageBucket: "fitopolis-72025.appspot.com",
+  messagingSenderId: "672295851701",
+  appId: "1:672295851701:web:578e4f7a83aa834d4b50fc",
+  measurementId: "G-DZK3LR9P9G",
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 const storage = firebase.storage;
+// const auth = getAuth();
 
 export { db, storage };
-

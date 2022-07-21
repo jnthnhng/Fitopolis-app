1f9fe216d71d270539ee2410185aa4bed9983f93
Jonathan Hang <hangjo@engr.orst.edu>
2022-07-20 20:30:11 -0500

    Running chip is now currently showing the correct results in the layout.

--- components/FitnessChips.js
+++ components/FitnessChips.js
@@ -15,89 +15,95 @@ const SearchChips = () => {
   // console.log(typeof query)
 
   return (
-    <View style={styles.chipContainer}>
-      <View style={styles.chip}>
-        <Chip
-          icon="bike"
-          mode="outlined"
-          style={{ backgroundColor: '#FF968A' }}
-          // onPress={setTheQuery('Cycling')}
-        >
-          Cycling
-        </Chip>
+    <>
+      <View style={styles.chipContainer}>
+        <View style={styles.chip}>
+          <Chip
+            icon="bike"
+            mode="outlined"
+            style={{ backgroundColor: '#FF968A' }}
+            // onPress={setTheQuery('Cycling')}
+          >
+            Cycling
+          </Chip>
+        </View>
+        <View style={styles.chip}>
+          <Chip
+            icon="swim"
+            mode="outlined"
+            style={{ backgroundColor: '#97C1a9' }}
+            // onPress={<GetChallenges searchType="Swimming" />}
+          >
+            Swimming
+          </Chip>
+        </View>
+        <View style={styles.chip}>
+          <Chip
+            icon="run"
+            mode="outlined"
+            style={{ backgroundColor: '#ffffb5' }}
+            selectedColor="black"
+            onPress={() => setQuery('Running')}
+          >
+            Running
+          </Chip>
+        </View>
+        <View style={styles.chip}>
+          <Chip
+            icon="dumbbell"
+            mode="outlined"
+            style={{ backgroundColor: '#ABDEE6' }}
+            // onPress={<GetChallenges searchType="Weightlifting" />}
+          >
+            Weightlifting
+          </Chip>
+        </View>
+        <View style={styles.chip}>
+          <Chip
+            icon="yoga"
+            mode="outlined"
+            style={{ backgroundColor: '#AAC5E2' }}
+            // onPress={<GetChallenges searchType="Yoga" />}
+          >
+            Yoga
+          </Chip>
+        </View>
+        <View style={styles.chip}>
+          <Chip
+            icon="walk"
+            mode="outlined"
+            style={{ backgroundColor: '#C2778B' }}
+            // onPress={<GetChallenges searchType="Aerobics" />}
+          >
+            Aerobics
+          </Chip>
+        </View>
+        <View style={styles.chip}>
+          <Chip
+            icon="run-fast"
+            mode="outlined"
+            style={{ backgroundColor: '#6891C3' }}
+            // onPress={<GetChallenges searchType="Cardio" />}
+          >
+            Cardio
+          </Chip>
+        </View>
       </View>
-      <View style={styles.chip}>
-        <Chip
-          icon="swim"
-          mode="outlined"
-          style={{ backgroundColor: '#97C1a9' }}
-          // onPress={<GetChallenges searchType="Swimming" />}
-        >
-          Swimming
-        </Chip>
+      <View style={styles.results}>
+        <GetChallenges searchType={query} />
       </View>
-      <View style={styles.chip}>
-        <Chip
-          icon="run"
-          mode="outlined"
-          style={{ backgroundColor: '#ffffb5' }}
-          selectedColor="black"
-          // onPress={() => setQuery('Running')}
-        >
-          Running
-        </Chip>
-      </View>
-      <View style={styles.chip}>
-        <Chip
-          icon="dumbbell"
-          mode="outlined"
-          style={{ backgroundColor: '#ABDEE6' }}
-          // onPress={<GetChallenges searchType="Weightlifting" />}
-        >
-          Weightlifting
-        </Chip>
-      </View>
-      <View style={styles.chip}>
-        <Chip
-          icon="yoga"
-          mode="outlined"
-          style={{ backgroundColor: '#AAC5E2' }}
-          // onPress={<GetChallenges searchType="Yoga" />}
-        >
-          Yoga
-        </Chip>
-      </View>
-      <View style={styles.chip}>
-        <Chip
-          icon="walk"
-          mode="outlined"
-          style={{ backgroundColor: '#C2778B' }}
-          // onPress={<GetChallenges searchType="Aerobics" />}
-        >
-          Aerobics
-        </Chip>
-      </View>
-      <View style={styles.chip}>
-        <Chip
-          icon="run-fast"
-          mode="outlined"
-          style={{ backgroundColor: '#6891C3' }}
-          // onPress={<GetChallenges searchType="Cardio" />}
-        >
-          Cardio
-        </Chip>
-      </View>
-      <View>
-        {/* <GetChallenges searchType={query} /> */}
-      </View>
-    </View>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
   chipContainer: {
+    flex: 1,
     flexWrap: 'wrap',
     flexDirection: 'row',
+  },
+  results: {
+    flex: 1,
   },
   chip: {
     // width: 120,

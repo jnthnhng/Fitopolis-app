abe8ceef29e7e375c54429a97551a89bf2acfed4
guedead <guedead@oregonstate.edu>
2022-07-26 08:13:23 -0500

    updating where favorite challenges points to

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -8,13 +8,13 @@ import {
   ScrollView,
   Image,
   Button,
-} from 'react-native';
+} from "react-native";
 
 // Database imports
-import { getAuth } from 'firebase/auth';
-import 'firebase/compat/storage';
-import firebase from 'firebase/compat/app';
-import { db, storage, firebaseConfig } from '../database/firebase.js';
+import { getAuth } from "firebase/auth";
+import "firebase/compat/storage";
+import firebase from "firebase/compat/app";
+import { db, storage, firebaseConfig } from "../database/firebase.js";
 import {
   set,
   update,
@@ -24,13 +24,13 @@ import {
   get,
   child,
   push,
-} from 'firebase/database';
-import GetFavorites from '../components/GetFavorites';
-import ListResults from '../components/ListResultsComponent.js';
+} from "firebase/database";
+import GetFavorites from "../components/GetFavorites";
+import ListResults from "../components/ListResultsComponent.js";
 
 // Other imports
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
@@ -38,7 +38,7 @@ if (!firebase.apps.length) {
 
 const FavoritesScreen = ({ navigation }) => {
   const [favorites, setFavorites] = useState(null);
-  const [user, setUser] = useState('');
+  const [user, setUser] = useState("");
   const [challenges, setChallenges] = useState([]);
   const [rawChallenges, setRawChallenges] = useState([]);
 
@@ -51,10 +51,10 @@ const FavoritesScreen = ({ navigation }) => {
   };
 
   const getFavorites = () => {
-    get(ref(db, 'users/' + userID + '/favorites/')).then((snapshot) => {
+    get(ref(db, "users/" + userID + "/favorites/")).then((snapshot) => {
       if (snapshot.exists()) {
         snapshot.forEach((element) => {
-          console.log('fave element: ', element.val().challenge);
+          console.log("fave element: ", element.val().challenge);
           getChallengeInfo(element.val().challenge);
         });
       }
@@ -62,45 +62,43 @@ const FavoritesScreen = ({ navigation }) => {
   };
 
   const getChallengeInfo = (challengePath) => {
-    get(ref(db, 'challenge/' + challengePath)).then((snapshot) => {
+    get(ref(db, "challenge/" + challengePath)).then((snapshot) => {
       if (snapshot.exists()) {
-        console.log('challenge snapshot: ', snapshot);
+        console.log("challenge snapshot: ", snapshot);
         addChallengeToEnd(snapshot);
       }
     });
   };
 
   useEffect(() => {
-    console.log('NEW');
+    console.log("NEW");
     getFavorites();
   }, []);
 
-  // Navigate to Participate screen
-
-  // function goToParticipate(item) {
-  //   console.log("ITEM: ", item)
-  //   // navigation.navigate('Participate', { challenges: item });
-  // }
 
   // MOCK to add favorites to user accounts - will need to remove
-  const addFavorite = () => {
-    // Mock creating a challenge, need to hook this up to the Challenge screen
-    const challengeId = 'Aerobics/' + '-N7Vaz_2b6FDT2pcsFfp';
-    const db = getDatabase();
-    // Create database reference
-    const postListRef = ref(
-      db,
-      'users/' + auth.currentUser.uid + '/favorites/'
-    );
-    const newPostRef = push(postListRef);
-    // Set child as challenge ID
-    set(newPostRef, {
-      challenge: challengeId,
-    });
-  };
+  // const addFavorite = () => {
+  //   // Mock creating a challenge, need to hook this up to the Challenge screen
+  //   const challengeId = "Aerobics/" + "-N7Vaz_2b6FDT2pcsFfp";
+  //   const db = getDatabase();
+  //   // Create database reference
+  //   const postListRef = ref(
+  //     db,
+  //     "users/" + auth.currentUser.uid + "/favorites/"
+  //   );
+  //   const newPostRef = push(postListRef);
+  //   // Set child as challenge ID
+  //   set(newPostRef, {
+  //     challenge: challengeId,
+  //   });
+  // };
 
   const renderItem = ({ item }) => (
-    <TouchableOpacity key={item.key} style={styles.item} onPress={() => navigation.navigate('Participate', {challenges: item})} >
+    <TouchableOpacity
+      key={item.key}
+      style={styles.item}
+      onPress={() => navigation.navigate("Participate", { challenges: item })}
+    >
       <Text style={styles.itemHeader}>{item.val().challengeName}</Text>
       <Text style={styles.itemDescription}>{item.val().description}</Text>
     </TouchableOpacity>
@@ -112,9 +110,6 @@ const FavoritesScreen = ({ navigation }) => {
         <View style={styles.header}>
           <Ionicons name="star" size={50} color="#7f03fc" />
           <Text style={styles.favorites}>Favorites</Text>
-        </View>
-        <View>
-          <Button title="Add Favorite" onPress={addFavorite} />
         </View>
         <View>
           <FlatList data={challenges} renderItem={renderItem} />
@@ -129,50 +124,50 @@ export default FavoritesScreen;
 const styles = StyleSheet.create({
   headerContainer: {
     flex: 1,
-    backgroundColor: '#e6e4df',
-    alignItems: 'center',
+    backgroundColor: "#e6e4df",
+    alignItems: "center",
   },
   container: {
     flex: 4,
     paddingTop: 10,
-    backgroundColor: 'red',
-    width: '95%',
+    backgroundColor: "red",
+    width: "95%",
   },
   item: {
-    backgroundColor: '#f6ebfc',
+    backgroundColor: "#f6ebfc",
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
     borderRadius: 10,
     elevation: 2,
-    shadowColor: 'black',
+    shadowColor: "black",
     shadowOpacity: 0.25,
   },
   itemHeader: {
     fontSize: 15,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   itemDescription: {
     fontSize: 12,
-    fontStyle: 'italic',
+    fontStyle: "italic",
   },
   header: {
-    alignItems: 'center',
+    alignItems: "center",
     marginTop: 20,
   },
   favorites: {
-    color: 'black',
+    color: "black",
     // fontFamily: 'Lato_700Bold',
-    fontWeight: 'bold',
+    fontWeight: "bold",
     fontSize: 50,
     marginBottom: 20,
   },
   button: {
-    backgroundColor: '#3b3a39',
-    width: '70%',
+    backgroundColor: "#3b3a39",
+    width: "70%",
     padding: 10,
     borderRadius: 10,
-    alignItems: 'center',
+    alignItems: "center",
     marginTop: 10,
   },
 });

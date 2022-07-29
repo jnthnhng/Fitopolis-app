656dd801e86541447946bcb65fc2863808cdfd97
guedead <guedead@oregonstate.edu>
2022-07-29 16:45:19 -0500

    cleaned up look and feel of milestone screen

--- screens/StatsScreen.js
+++ screens/StatsScreen.js
@@ -23,89 +23,12 @@ if (!firebase.apps.length) {
 }
 
 const FavoritesScreen = ({ navigation }) => {
-  const [challenges, setChallenges] = useState([]);
-
-  const auth = getAuth();
-  const userID = auth.currentUser.uid;
-  const db = getDatabase();
-
   // This adds the favorite to the end of the challenges array
-  const addChallengeToEnd = (newChallenge) => {
-    setChallenges((state) => [...state, newChallenge]);
-  };
-
-  const getCreated = () => {
-    // Get favorites from user ID on realtime database
-    get(ref(db, "users/" + userID + "/created/")).then((snapshot) => {
-      // Loop through them and get the challenge information from each favorited item
-      // These are stored in the challenges array
-      if (snapshot.exists()) {
-        snapshot.forEach((element) => {
-          console.log("fave element: ", element.val().challenge);
-          getChallengeInfo(element.val().challenge);
-        });
-      }
-    });
-  };
-
-  // Retrieves challenge object from the path saved in user favorites
-  const getChallengeInfo = (challengePath) => {
-    get(ref(db, "challenge/" + challengePath)).then((snapshot) => {
-      if (snapshot.exists()) {
-        console.log("challenge snapshot: ", snapshot);
-        addChallengeToEnd(snapshot);
-      }
-    });
-  };
-
-  useEffect(() => {
-    getCreated();
-  }, []);
-
-  // MOCK to add favorites to user accounts - will need to remove
-  // const addFavorite = () => {
-  //   // Mock creating a challenge, need to hook this up to the Challenge screen
-  //   const challengeId = "Aerobics/" + "-N7Vaz_2b6FDT2pcsFfp";
-  //   const db = getDatabase();
-  //   // Create database reference
-  //   const postListRef = ref(
-  //     db,
-  //     "users/" + auth.currentUser.uid + "/favorites/"
-  //   );
-  //   const newPostRef = push(postListRef);
-  //   // Set child as challenge ID
-  //   set(newPostRef, {
-  //     challenge: challengeId,
-  //   });
-  // };
-
-  // Renders flatlist item
-  const renderItem = ({ item }) => (
-    <TouchableOpacity
-      key={item.key}
-      style={styles.item}
-      onPress={() => navigation.navigate("Participate", { challenges: item })}
-    >
-      <Text style={styles.itemHeader}>{item.val().challengeName}</Text>
-      <Text style={styles.itemDescription}>{item.val().description}</Text>
-    </TouchableOpacity>
-  );
 
   return (
     <>
       <View style={styles.headerContainer}>
-        <View style={styles.header}>
-          <Ionicons name="stats-chart" size={50} color="#7f03fc" />
-          <Text style={styles.favorites}>My Challenges</Text>
-        </View>
-        <SafeAreaView>
-          <Text>CREATED CHALLENGES</Text>
-          <FlatList data={challenges} renderItem={renderItem} />
-        </SafeAreaView>
-        <SafeAreaView>
-          <Text>IN PROGRESS CHALLENGES</Text>
-
-        </SafeAreaView>
+        <Text>Will be Chat</Text>
       </View>
     </>
   );
